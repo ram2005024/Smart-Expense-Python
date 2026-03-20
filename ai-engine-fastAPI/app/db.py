2d49d722dd45a0695cbcbbff466f7a3c83e940a5
ram2005024 <sharmashekhar20050@gmail.com>
2026-03-20 14:20:02 +0545

    Initial

--- ai-engine-fastAPI/app/db.py
+++ ai-engine-fastAPI/app/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
 # Local Postgres connection
-DATABASE_URL = "postgresql://myuser:9crqoovg9@localhost:5432/smart_expenses"
+DATABASE_URL = "postgresql://myuser:9crqoovg9@localhost:5432/smart_expense"
 
 engine = create_engine(DATABASE_URL)
