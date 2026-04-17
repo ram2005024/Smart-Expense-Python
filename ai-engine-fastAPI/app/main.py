af878f735c76e2f6f32e6d0bee9040bf6ef575b8
ram2005024 <sharmashekhar20050@gmail.com>
2026-04-17 23:58:31 +0545

    initial

--- ai-engine-fastAPI/app/main.py
+++ ai-engine-fastAPI/app/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
-from .services import budget_insights,expense_insights,expense_trend,expense_anomaly,spend_prediction
+from .services import (
+    budget_insights,
+    expense_insights,
+    expense_trend,
+    expense_anomaly,
+)
 from sqlalchemy import create_engine
 
-DATABASE_URL="postgresql://myuser:9crqoovg9@localhost:5432/smart_expenses"
+DATABASE_URL = "postgresql://myuser:9crqoovg9@localhost:5432/smart_expenses"
 engine = create_engine(DATABASE_URL)
 
-app=FastAPI()
+app = FastAPI()
 
 # Include routers in main
 
-app.include_router(budget_insights.router,prefix="/ai",tags=['Budget-Insights'])
-app.include_router(expense_insights.router,prefix="/ai",tags=["Expense-Insight"])
-app.include_router(expense_trend.router,prefix="/ai",tags=["Expense-Trend-Analyse"])
-app.include_router(expense_anomaly.router,prefix="/ai",tags=["Expense-Anomaly-Detection"])
-app.include_router(spend_prediction.router,prefix="/ai",tags=["Budget-Expense-Prediction"])
+app.include_router(budget_insights.router, prefix="/ai", tags=["Budget-Insights"])
+app.include_router(expense_insights.router, prefix="/ai", tags=["Expense-Insight"])
+app.include_router(expense_trend.router, prefix="/ai", tags=["Expense-Trend-Analyse"])
+app.include_router(
+    expense_anomaly.router, prefix="/ai", tags=["Expense-Anomaly-Detection"]
+)
