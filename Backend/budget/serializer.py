d45ea526f41bdbefabf74ce5d0e179eefa836dd8
ram2005024 <sharmashekhar20050@gmail.com>
2026-04-22 15:44:00 +0545

    Backend functionality for budget showcase block

--- Backend/budget/serializer.py
+++ Backend/budget/serializer.py
@@ -3,22 +3,22 @@ from rest_framework import serializers
 from expense.models import Budget
 from django.db.models import Sum
 from expense.serializer import ExpenseSerializer
+
 # Budget serializer
 
+
 class BudgetSerializer(serializers.ModelSerializer):
-    transactions=serializers.SerializerMethodField()
-    remaining=serializers.SerializerMethodField()
-    total_spent=serializers.SerializerMethodField()
-    usage_percentage=serializers.SerializerMethodField()
-    budget_status=serializers.SerializerMethodField()
-    is_active=serializers.SerializerMethodField()
+    transactions = serializers.SerializerMethodField()
+    remaining = serializers.SerializerMethodField()
+    total_spent = serializers.SerializerMethodField()
+    usage_percentage = serializers.SerializerMethodField()
+    budget_status = serializers.SerializerMethodField()
+    is_active = serializers.SerializerMethodField()
     # budget_expenses=ExpenseSerializer(many=True,read_only=True)
-    
-    
-    
+
     class Meta:
-        model=Budget
-        fields=[
+        model = Budget
+        fields = [
             "budget_name",
             "budget_limit",
             "budget_field",
@@ -33,39 +33,43 @@ class BudgetSerializer(serializers.ModelSerializer):
             "remaining",
             "transactions",
             "usage_percentage",
-            "budget_status"
+            "budget_status",
         ]
 
     # GET custom field
-    def get_transactions(self,obj):
+    def get_transactions(self, obj):
         return obj.budget_expenses.count()
-    
-    def get_total_spent(self,obj):
-        return obj.budget_expenses.aggregate(
-            total=Sum("expense_amount")
-        )["total"] or 0
-    def get_remaining(self,obj):
-        total_spent=self.get_total_spent(obj)
-        return 0 if obj.budget_amount-total_spent<0 else obj.budget_amount-total_spent
-    
-    def get_usage_percentage(self,obj):
-        total_spent=self.get_total_spent(obj)
-        return round((total_spent/obj.budget_amount)*100,2)
-    
-    def get_budget_status(self,obj):
-        budget_usage_percentage=self.get_usage_percentage(obj)
-         # Budget Status
-        budget_status=""
+
+    def get_total_spent(self, obj):
+        return obj.budget_expenses.aggregate(total=Sum("expense_amount"))["total"] or 0
+
+    def get_remaining(self, obj):
+        total_spent = self.get_total_spent(obj)
+        return (
+            0
+            if obj.budget_amount - total_spent < 0
+            else obj.budget_amount - total_spent
+        )
+
+    def get_usage_percentage(self, obj):
+        total_spent = self.get_total_spent(obj)
+        return round(
+            (total_spent / obj.budget_amount if obj.budget_amount > 0 else 0) * 100, 2
+        )
+
+    def get_budget_status(self, obj):
+        budget_usage_percentage = self.get_usage_percentage(obj)
+        # Budget Status
+        budget_status = ""
         if budget_usage_percentage < 50:
-            budget_status="On track"
-        elif budget_usage_percentage>=50 and budget_usage_percentage <100:
-            budget_status="Near limit"
+            budget_status = "On track"
+        elif budget_usage_percentage >= 50 and budget_usage_percentage < 100:
+            budget_status = "Near limit"
         else:
-            budget_status="⚠️ Over budget"
-            
+            budget_status = "⚠️ Over budget"
+
         return budget_status
-    
-    def get_is_active(self,obj):
-        today=datetime.date.today()
-        return obj.valid_until and today<obj.valid_until
-        
+
+    def get_is_active(self, obj):
+        today = datetime.date.today()
+        return obj.valid_until and today < obj.valid_until
